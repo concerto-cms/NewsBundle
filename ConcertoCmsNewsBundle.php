54edc68cc4e7d5df5f94069b386327b0971e0b6c
Mathieu Maes <mathieu@zimmo.be>
2014-05-23 15:59:21 +0200

    initial commit

---
+++ ConcertoCmsNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ConcertoCms\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ConcertoCmsNewsBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ConcertoCms\NewsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ConcertoCmsNewsBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
